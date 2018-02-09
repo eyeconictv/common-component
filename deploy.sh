03aecdec59a992ce2bbb7ef7304b4afa14088ac1
rodrigopavezi <rodrigopavezi@gmail.com>
2018-02-09 15:16:15 -0200

    Change componets bucket to rise-content

--- deploy.sh
+++ deploy.sh
@@ -12,25 +12,21 @@ if [ "$BRANCH" = "STABLE" ]; then MANIFESTFILES="display-modules-*.json"; fi
 if [ "$BRANCH" = "STABLE" ]; then ROLLOUTPCT="0"; fi
 
 echo "deploying $VERSION"
-echo "deploying to install-versions.risevision.com/$OUTPUTDIR"
+echo "deploying to rise-content/$OUTPUTDIR"
 
 git clone git@github.com:Rise-Vision/private-keys.git
 gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ./private-keys/storage-server/rva-media-library-ce0d2bd78b54.json
 find manifests -name "*.json" -exec node ./common-component/update-component-version.js '{}' $COMPONENTNAME $VERSION 0 \;
 
-gsutil -m cp manifests/*.json gs://install-versions.risevision.com/staging/components/$COMPONENTNAME/$VERSION
-gsutil setmeta -h "Cache-Control:private, max-age=0" gs://install-versions.risevision.com/staging/components/$COMPONENTNAME/$VERSION/*
-gsutil setmeta -h "Content-Disposition:attachment" gs://install-versions.risevision.com/staging/components/$COMPONENTNAME/$VERSION/*.sh
-gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/staging/components/$COMPONENTNAME/$VERSION/*
-gsutil -m cp -p gs://install-versions.risevision.com/${OUTPUTDIR}*.{sh,exe,json} gs://install-versions.risevision.com/backups/$VERSION
-gsutil -m cp -p gs://install-versions.risevision.com/staging/components/$COMPONENTNAME/$VERSION/* gs://install-versions.risevision.com/releases/components/$COMPONENTNAME/$VERSION
-gsutil -m cp -p gs://install-versions.risevision.com/staging/components/$COMPONENTNAME/$VERSION/* gs://install-versions.risevision.com/$OUTPUTDIR
+gsutil -m cp manifests/*.json gs://rise-content/staging/components/$COMPONENTNAME/$VERSION
+gsutil setmeta -h "Cache-Control:private, max-age=0" gs://rise-content/staging/components/$COMPONENTNAME/$VERSION/*
+gsutil setmeta -h "Content-Disposition:attachment" gs://rise-content/staging/components/$COMPONENTNAME/$VERSION/*.sh
+gsutil acl ch -u AllUsers:R gs://rise-content/staging/components/$COMPONENTNAME/$VERSION/*
+gsutil -m cp -p gs://rise-content/${OUTPUTDIR}*.{sh,exe,json} gs://rise-content/backups/$VERSION
+gsutil -m cp -p gs://rise-content/staging/components/$COMPONENTNAME/$VERSION/* gs://rise-content/releases/components/$COMPONENTNAME/$VERSION
+gsutil -m cp -p gs://rise-content/staging/components/$COMPONENTNAME/$VERSION/* gs://rise-content/$OUTPUTDIR/components/$COMPONENTNAME
 
 if [ "$BRANCH" = "STABLE" ]
 then
-  echo -n "RisePlayerElectron $VERSION" > latest-version
-  gsutil cp latest-version gs://install-versions.risevision.com
-  gsutil setmeta -h "Cache-Control:private, max-age=0" gs://install-versions.risevision.com/latest-version
-  gsutil setmeta -h "Content-Type:text/plain" gs://install-versions.risevision.com/latest-version
-  gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/latest-version
+  gsutil -m cp -p gs://rise-content/staging/components/$COMPONENTNAME/$VERSION/* gs://rise-content/stable/components/$COMPONENTNAME
 fi
